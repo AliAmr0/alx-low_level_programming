26ae94816876608b5697aa11eabd8b1ee51a95f5
Ali Amr <aliamreducation@gmail.com>
2023-06-16 15:15:16 +0300

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,9 @@ int main(void)
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
-		if (x != 'q' || x != 'e')
+		if (x == 'q' || x == 'e')
+			continue;
+		else
 			putchar(x);
 	putchar('\n');
 	return (0);
