a48c8d99c451f3a29fbb12fe73a5d367c0f22226
Ali Amr <aliamreducation@gmail.com>
2023-06-26 13:42:05 +0300

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,12 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i, count = 0;
 
-	for (i = -1; *(s + i) != 0; i--)
-	{
+	for (i = 0; *(s + i) != 0; i++)
+		count++;
+
+	for (i = count - 1; i >= 0; i--)
 		_putchar(*(s + i));
-	}
+	_putchar('\n');
 }
