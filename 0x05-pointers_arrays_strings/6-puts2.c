afb419dd370af0f52a72d7d3dc5d4d5059965a52
AliAmr <aliamreducation@gmail.com>
2023-06-26 03:53:48 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2-> a function to print every other character of a string
+ *
+ * @str: str is a pointer to string parameter
+ *
+ * Return: void (co return)
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != 0; i + 2)
+	{
+		_putchar(*(str + i))
+	}
+}
